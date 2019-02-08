f8bbe8de35d9919d9d4381a45da5619a8364b2a9
Mequanent <shifem@uw.edu>
2019-02-07 23:15:37 -0800

    com

--- analysis.R
+++ analysis.R
@@ -1,22 +1,33 @@
 # Analysis script: compute values and create graphics of interest
 library("dplyr")
+#install.packages("ggplot2")
+#install.packages("lubridate")
+
 library("ggplot2")
 library("lubridate")
 library("tidyr")
 library("ggmap")
 
 # Load in your data
-
+evictions <- read_csv("data/Eviction_Notices.csv", stringsAsFactors = FALSE)
 # Compute some values of interest and store them in variables for the report
 
 # How many evictions were there?
-
+num_evic <- nrow(evictions)
+num_fetures <- ncol(evictions)
 # Create a table (data frame) of evictions by zip code (sort descending)
-
+ by_zip <- evictions %>% 
+    group_by(Eviction_Notices.Source.Zipcode) %>% 
+   count() %>% arrange(-n) %>%  top_n(10, wt = n)
 # Create a plot of the number of evictions each month in the dataset
-
+ by_month <- evictions %>%  mutate(date = as.Date(file,date, format = "%M/%d/%y")) %>% 
+   mutate(mounth = floor_date(date, unit = "month")) %>%  group_by(mounth) %>% 
+   count() %>% 
+   
+ 
 # Store plot in a variable
-
+ ggplot(data = by_month) + geom_line(mapping = aes(x = month, y = n)) + 
+   label_both(x = "Date", y = "Number", title = "evictions over time in SF")
 # Map evictions in 2017 
 
 # Format the lat/long variables, filter to 2017
