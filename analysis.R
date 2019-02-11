b1b3e3ee4ae563fd05bd151a27abf02376eec653
Mequanent <shifem@uw.edu>
2019-02-10 20:28:11 -0800

    comm

--- analysis.R
+++ analysis.R
@@ -16,18 +16,18 @@ evictions <- read_csv("data/Eviction_Notices.csv", stringsAsFactors = FALSE)
 num_evic <- nrow(evictions)
 num_fetures <- ncol(evictions)
 # Create a table (data frame) of evictions by zip code (sort descending)
- by_zip <- evictions %>% 
-    group_by(Eviction_Notices.Source.Zipcode) %>% 
-   count() %>% arrange(-n) %>%  top_n(10, wt = n)
+by_zip <- evictions %>% 
+  group_by(Eviction_Notices.Source.Zipcode) %>% 
+  count() %>% arrange(-n) %>%  top_n(10, wt = n)
 # Create a plot of the number of evictions each month in the dataset
- by_month <- evictions %>%  mutate(date = as.Date(file,date, format = "%M/%d/%y")) %>% 
-   mutate(mounth = floor_date(date, unit = "month")) %>%  group_by(mounth) %>% 
-   count() %>% 
-   
- 
-# Store plot in a variable
- ggplot(data = by_month) + geom_line(mapping = aes(x = month, y = n)) + 
-   label_both(x = "Date", y = "Number", title = "evictions over time in SF")
+by_month <- evictions %>%  mutate(date = as.Date(file,date, format = "%M/%d/%y")) %>% 
+  mutate(mounth = floor_date(date, unit = "month")) %>%  group_by(mounth) %>% 
+  count() %>% 
+  
+  
+  # Store plot in a variable
+  ggplot(data = by_month) + geom_line(mapping = aes(x = month, y = n)) + 
+  label_both(x = "Date", y = "Number", title = "evictions over time in SF")
 # Map evictions in 2017 
 
 # Format the lat/long variables, filter to 2017
